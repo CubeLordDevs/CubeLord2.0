6b06a616c7448ba7945c4a5fac4e6180ad0b5f2c
Dodesimo <devammondal06@gmail.com>
2020-06-17 23:27:17 -0400

    Embeds are added for all commands except for "+unban" (bug occurs, and must be fixed.)

--- cubelord2.0.py
+++ cubelord2.0.py
@@ -19,7 +19,7 @@ async def on_ready():
 @client.command()
 async def ping(ctx):
     embed = discord.Embed(
-    colour = discord.Colour.dark_red(),
+    colour = discord.Colour.green(),
     title = "Ping-Pong Latency Test",
     )
     embed.add_field(name="Results:", value= f'Pong! ```Latency: {round(client.latency * 1000)} milliseconds``` ')
@@ -56,7 +56,7 @@ async def _8ball(ctx, *, question):
     ]
 
     embed = discord.Embed(
-    colour=discord.Colour.dark_red(),
+    colour=discord.Colour.green(),
     title="8Ball",
     )
     embed.add_field(name="Question:", value=f'{question}')
@@ -108,35 +108,73 @@ async def on_command_error(ctx, error):
 @client.command()
 @has_permissions(manage_roles=True, kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason=None):
-    await ctx.send(f'User {member.mention} has been kicked due to: {reason}.')
+
+    embed = discord.Embed(
+        colour=discord.Colour.green(),
+        title="Kick Successful!"
+    )
+    embed.add_field(name="Results:", value=f'User {member.mention} has been kicked due to: {reason}.')
     await member.kick(reason=reason)
+    await ctx.send(embed=embed)
 
 # Number 2: Ban command (+ban <user> <reason>)
 @client.command()
 @has_permissions(manage_roles=True, ban_members=True)
 async def ban(ctx, member: discord.Member, *, reason=None):
+
+    embed = discord.Embed(
+        colour=discord.Colour.green(),
+        title="Ban Successful!"
+    )
     await member.ban(reason=reason)
-    await ctx.send(f"User {member.mention} has been banned due to: {reason}.")
+    embed.add_field(name="Results:", value=f"User {member.mention} has been banned due to: {reason}.")
+    await ctx.send(embed=embed)
 
 # Number 3: Unban command (+unban <user> <reason)
 @client.command()
 async def unban(ctx, *, member):
+
     banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split('# ')
+    member_name, member_discriminator = member.split('#')
 
     for ban_entry in banned_users:
         user = ban_entry.user
 
         if(user.name, user.discriminator) == (member_name, member_discriminator):
+            embed = discord.Embed(
+                colour=discord.Colour.green(),
+                title="Ban Successful!"
+            )
             await ctx.guild.unban(user)
-            await ctx.send(f'{user.mention} has been unbanned.')
-            return
+            embed.add_field(name="Results", value="f'{user.mention} has been unbanned.'")
+            await ctx.send(embed=embed)
+
 
 # Number 4: Find userID of user
 @client.command()
 async def userid(ctx, user: discord.User):
     id = user.id
-    await ctx.send(f"{str(user)}'s User ID is: {id}.")
+
+    embed = discord.Embed(
+        colour=discord.Colour.green(),
+        title=f"{user.display_name}'s User ID"
+    )
+    embed.add_field(name=f"ID: ", value=f"{str(user)}'s User ID is: **{id}**.")
+    embed.set_thumbnail(url=user.avatar_url)
+    await ctx.send(embed=embed)
+
+# Number 5: Display avatar of mentioned user
+@client.command()
+async def avatar(ctx, user: discord.User):
+    avatar = user.avatar_url
+
+    embed = discord.Embed(
+        colour=discord.Colour.green(),
+        title=f"Avatar Lookup"
+    )
+    embed.add_field(name=f"{user.display_name}'s Avatar", value=f"This is {user.display_name}'s Avatar:")
+    embed.set_image(url=avatar)
+    await ctx.send(embed=embed)
 
 # Run the bot.
 # Note: Store token in external file later for security reasons
