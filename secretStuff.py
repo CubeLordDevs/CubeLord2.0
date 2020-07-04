3c13d87db32cba32f81e4130f07e7fe9a97e52e8
Dodesimo <devammondal06@gmail.com>
2020-07-04 11:04:41 -0400

    token change

--- secretStuff.py
+++ secretStuff.py
@@ -3,7 +3,7 @@ secretStuff = dict(
     client_id = "lFuFwceN0o_b9w",
     client_secret = "asLLFB4ycCr927RIcvR_CSr4GBs",
     user_agent = "discord:CubeLord2.0:1.00:(by u/Dodesimo)",
-    token = "NzE5NzQyNDEyMTg4MDI0OTU0.XubgEw.Ub79PYYmYGDVVVhQKqhdA7hNq3U"
+    token = "NzE5NzQyNDEyMTg4MDI0OTU0.XwCZlw.j-lIlEw3QG8iBnMXWU-fKA9w0lg"
 
 )
 
