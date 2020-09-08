cd4fb68768b71178678e3e17182925c53755df91
Dodesimo <devammondal06@gmail.com>
2020-09-07 22:04:38 -0400

    Credentials update.

--- cubelord.py
+++ cubelord.py
@@ -2,5 +2,5 @@ cubelord = dict(
     client_id = "lFuFwceN0o_b9w",
     client_secret = "asLLFB4ycCr927RIcvR_CSr4GBs",
     user_agent = "discord:CubeLord2.0:1.00:(by u/Dodesimo)",
-    token = "NzE5NzQyNDEyMTg4MDI0OTU0.Xw85Ag.xppqz4KUODGi8gqr40a-U89-whI"
+    token = "NzE5NzQyNDEyMTg4MDI0OTU0.Xt72Ww.QURPEgseW7z0xmWFh_Pz8wuq3vY"
 )
