43f228ef879cfeb42f0bdf387de52a1b49e726eb
akirarorschach <69493313+akirarorschach@users.noreply.github.com>
2020-09-07 22:04:03 -0400

    Delete cubelord.py

--- cubelord.py
+++
@@ -1,6 +0,0 @@
-cubelord = dict(
-    client_id = "lFuFwceN0o_b9w",
-    client_secret = "asLLFB4ycCr927RIcvR_CSr4GBs",
-    user_agent = "discord:CubeLord2.0:1.00:(by u/Dodesimo)",
-    token = "NzE5NzQyNDEyMTg4MDI0OTU0.Xw85Ag.xppqz4KUODGi8gqr40a-U89-whI"
-)
